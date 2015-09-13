3b007a255d987999a94167b4d674ba48e0a32761
Richard Kirchofer <rgkirch@gmail.com>
2015-09-13 19:32:39 -0400

    i added a command bar

--- bentBrowser.py
+++ bentBrowser.py
@@ -7,14 +7,33 @@ import os
 
 # main widget
 class Listing( object ):
+    command_history = []
+    command_history_index = 0
     def __init__(self, parent):
         self.lbx_dirs = tk.Listbox( parent,
                 selectmode=tk.EXTENDED )
-        self.lbx_dirs.grid()
+        self.lbx_dirs.grid( row=0 )
+
+        self.command_bar = tk.Entry( parent )
+        self.command_bar.bind( "<Return>", self.run_command )
+        self.command_bar.bind( "<Up>", self.display_previous_command )
+        self.command_bar.grid( row=10 )
+
         self.update()
+    def run_command( self, event ):
+        "record entered command in history, reset history index, and run command"
+        print( self.command_bar.get() )
+        self.command_history.append( self.command_bar.get() )
+        self.command_history_index = len( self.command_history )
+        self.command_bar.delete( 0, tk.END )
+    def display_previous_command( self, event ):
+        pass
     def update(self):
+        self.lbx_dirs.delete( 0, tk.END )
         for item in sorted(os.listdir( os.path.expanduser("~") + "/" )):
             self.lbx_dirs.insert(tk.END, item)
+    def debug_print_selected( self ):
+        print( self.lbx_dirs.curselection() )
 
 class BentExplorerApp( tk.Tk ):
     def __init__( self, *args, **kwargs ):
@@ -23,6 +42,7 @@ class BentExplorerApp( tk.Tk ):
 
         self.wm_title("bent file explorer")
         #print( self.winfo_screenwidth() )
+        # if you leave out the master widget as seen here, tkinter uses the most recently created root window as master
         frame = tk.Frame()
         frame.grid( row=5 )
         bottom = tk.Frame()
