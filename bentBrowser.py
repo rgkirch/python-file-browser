283323e6e885e181078f3273ebac36112e050456
Richard Kirchofer <rgkirch@gmail.com>
2015-09-13 17:24:53 -0400

    works so far

--- bentBrowser.py
+++ bentBrowser.py
@@ -2,40 +2,43 @@
 
 #from Tkinter import *
 import Tkinter as tk
+import tkMessageBox
 import os
 
 # main widget
 class Listing( object ):
     def __init__(self, parent):
-        self.title_text = tk.Message( parent,
-                text="bent file explorer" )
-
-
         self.lbx_dirs = tk.Listbox( parent,
                 selectmode=tk.EXTENDED )
-
-        self.title_text.grid()
         self.lbx_dirs.grid()
+        self.update()
     def update(self):
-        print( "update" )
-        for item in sorted(os.listdir(".")):
-            self.lbx_dirs.insert(END, item)
+        for item in sorted(os.listdir( os.path.expanduser("~") + "/" )):
+            self.lbx_dirs.insert(tk.END, item)
 
 class BentExplorerApp( tk.Tk ):
     def __init__( self, *args, **kwargs ):
         tk.Tk.__init__( self, *args, **kwargs )
+        self.protocol( "WM_DELETE_WINDOW", self.destroy )
+
+        self.wm_title("bent file explorer")
+        #print( self.winfo_screenwidth() )
         frame = tk.Frame()
-        frame.grid( row=0 )
+        frame.grid( row=5 )
         bottom = tk.Frame()
         bottom.grid( row=10 )
 
         btn_quit = tk.Button( bottom,
                 text="QUIT",
-                command=self.quit )
+                command=self.Quit )
         btn_quit.grid()
 
         widget_file_explorer = Listing( frame )
+    def Quit( self ):
+        if tkMessageBox.askokcancel("Quit", "Do you really wish to quit?"):
+            self.quit()
 
 app = BentExplorerApp()
 app.mainloop()
-app.destroy()
+#app.destroy()
+
